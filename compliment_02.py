ac0416390d09e1ecbdf54a74b4ccde917c6d850c
Alex Carfora <alexcarfora1997@gmail.com>
2025-10-02 10:15:15 +0000

    sub b#2

--- compliment_02.py
+++ compliment_02.py
@@ -18,9 +18,9 @@
 user_choice = input("Would you like a compliment?")
 if user_choice == "yes":
     print("You have wonderful eyes.")
-    print("Thank you for playing.")
 else:
     print("No compliment for you!")
+print("Thank you for playing.")
 
 
 
